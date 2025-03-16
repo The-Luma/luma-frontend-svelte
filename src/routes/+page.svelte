64e2c0b07953f97b08e01b773cb6bd8e3b70669b
Aleksandr <wigged_bitmap1s@icloud.com>
2025-03-15 21:25:17 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<input
-			class="input"
-			type="text"
-			placeholder="Enter email or username"
-			required
-		/>
+		
+		Hi, it took us long to get here.
 
 	</div>
 </div>
