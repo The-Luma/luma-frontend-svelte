bce17aa522511d89955993e0718777309d2a6b85
Aleksandr <wigged_bitmap1s@icloud.com>
2025-03-19 08:14:46 -0400

    Remove skeleton from tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import { join } from 'path'
 import type { Config } from 'tailwindcss'
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
 
 const { LumaOriginalTheme } = require('./src/lib');
 
@@ -14,18 +13,6 @@ export default {
 	},
 	plugins: [
 		forms,
-		typography,
-		skeleton({
-			themes: {
-				custom: [LumaOriginalTheme],
-				preset: [
-
-					{
-						name: 'skeleton',
-						enhancements: true,
-					},
-				],
-			},
-		}),
+		typography
 	],
 } satisfies Config;
