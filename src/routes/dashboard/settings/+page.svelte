f2ca4ab9134a25dbc2abaeeb5ab14708f989f853
kazneo <kazneo213@gmail.com>
2025-03-28 18:17:19 -0400

    added basic eleents for user setting page. need to add elements for admin users. need to make logic. need to check for css

--- src/routes/dashboard/settings/+page.svelte
+++ src/routes/dashboard/settings/+page.svelte
@@ -4,5 +4,13 @@
 
 <div class="card p-4">
     <h1 class="h1">Settings</h1>
-    <p class="text-surface-600-400">Settings management coming soon...</p>
+    <div class='relative size-32 ...'>
+        <p class="text-sm text-gray-500">email</p>
+        <p class="text-sm text-gray-500">username</p>
+        <button class="rounded-full ...">Change Username</button>
+        <p class="text-sm text-gray-500">password</p>
+        <button class="rounded-full ...">Change Password</button>
+        
+        <button class="rounded-full ...">Logout</button>
+    </div>
 </div> 
